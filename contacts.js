35c61bf233c6aa291e0ca9c5e24bbd9fc4559de2
Arsenii2-maker <georgievskiiarsenii@gmail.com>
2026-01-24 23:09:50 +1000

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,16 +1,14 @@
 const contacts = [
-  { name: 'Alex', avatar: 'https://i.pravatar.cc/100?img=12' },
-  { name: 'Mia', avatar: 'https://i.pravatar.cc/100?img=32' },
-  { name: 'Luna', avatar: 'https://i.pravatar.cc/100?img=47' }
+  { name: 'Мия', status: 'online' },
+  { name: 'Луна', status: 'offline' }
 ];
 
-function renderContacts() {
-  contactsList.innerHTML = '';
-  contacts.forEach(c => {
-    const div = document.createElement('div');
-    div.className = 'contact';
-    div.innerHTML = `<img class="avatar" src="${c.avatar}"><span>${c.name}</span>`;
-    div.onclick = () => openChat(c);
-    contactsList.appendChild(div);
-  });
-}
+const list = document.getElementById('contacts-list');
+
+contacts.forEach(c => {
+  const div = document.createElement('div');
+  div.className = 'contact';
+  div.innerHTML = `<strong>${c.name}</strong><span>${c.status}</span>`;
+  div.onclick = () => openChat(c);
+  list.appendChild(div);
+});

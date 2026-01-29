d8224f69c864cda5c8273d3a5e39de6b7ea12439
Arsenii2-maker <georgievskiiarsenii@gmail.com>
2026-01-29 12:09:42 +1000

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,3 +1,35 @@
+let currentMood = 'cozy'; // дефолт
+
+const moodSettings = {
+  cozy: {
+    bg: '#fff8fc',           // мягкий розовый
+    bubbleMe: '#ffadd6',
+    bubbleOther: '#ffe4f0',
+    sound: 'cozy-pop.mp3',   // мягкий пузырьковый chime
+    anim: 'cozy'
+  },
+  night: {
+    bg: '#0d1a2b',
+    bubbleMe: '#7ab8ff',
+    bubbleOther: '#2a3b52',
+    sound: 'night-chime.mp3', // тихий колокольчик
+    anim: 'night'
+  },
+  lofi: {
+    bg: '#fdfaf5',
+    bubbleMe: '#d9b99b',
+    bubbleOther: '#f0e6d9',
+    sound: 'lofi-ding.mp3',   // расслабленный ding
+    anim: 'lofi'
+  },
+  chaotic: {
+    bg: '#fff0f0',
+    bubbleMe: '#ff6b6b',
+    bubbleOther: '#ffecec',
+    sound: 'chaotic-boop.mp3', // быстрый игривый boop
+    anim: 'chaotic'
+  }
+};
 let currentChat = null;
 const chatData = {};
 

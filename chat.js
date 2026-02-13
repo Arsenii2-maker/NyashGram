a74b781ef786677a4d7b348b25f5474ba19726e1
Arsenii2-maker <georgievskiiarsenii@gmail.com>
2026-02-13 21:40:16 +1000

    Update chat.js

--- chat.js
+++ chat.js
@@ -45,7 +45,7 @@ const GEMINI_API_KEY = "AIzaSyCxwrtIx9r3t-EqgSuu-ZAP-VFoTbwU1K0"; // твой к
 async function getNyashGPTResponse(text) {
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {

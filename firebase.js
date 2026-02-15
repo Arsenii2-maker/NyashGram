7939f8276661f50f9d0703fe530d2a45f5d4a285
Arsenii2-maker <georgievskiiarsenii@gmail.com>
2026-02-15 14:56:52 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,6 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
 const firebaseConfig = {
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
   apiKey: "AIzaSyAEKx_zRsC6r24d-l82MmEKIT1Y92cGc-U",
   authDomain: "nyashgram.firebaseapp.com",
   projectId: "nyashgram",
@@ -20,11 +13,8 @@ const firebaseConfig = {
   appId: "1:590608200225:web:1b42cd61f7a0a00dbb6960"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-};
 
-const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
